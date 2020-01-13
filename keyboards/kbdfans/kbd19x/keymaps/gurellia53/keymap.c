eee58e82a508c54f88905e7747a37c3b384ae2d0
andrew <gurellia53@gmail.com>
2020-01-12 23:06:48 -0600

    delete key

--- keyboards/kbdfans/kbd19x/keymaps/gurellia53/keymap.c
+++ keyboards/kbdfans/kbd19x/keymaps/gurellia53/keymap.c
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_plus( /* Base */
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_HOME, KC_END,  KC_PGUP, KC_PGDN, \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_HOME, KC_END,  KC_PGUP, KC_PGDN, \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
     KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_P4,   KC_P5,   KC_P6,   KC_TAB,  \
@@ -26,12 +26,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),   KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,   KC_PDOT           \
   ),
   [1] = LAYOUT_split_plus( /* Func */
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  RGB_TOG,         _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_PSCR,         _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  RESET,           _______, _______, _______, _______, \
     _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,  _______,         _______, _______, _______, _______, \
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,         _______, _______, _______, _______, \
     KC_UNDO,          KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, _______, _______, _______, _______,     _______,   KC_VOLU,    _______, _______, _______, KC_CALC, \
-    _______, _______, _______,                            _______,                   _______, _______, _______,       _______, KC_VOLD, _______,    _______, _______           \
+    _______, _______, _______,                            _______,                   _______, _______, _______,       KC_MUTE, KC_VOLD, _______,    _______, _______           \
   ),
 };
 
