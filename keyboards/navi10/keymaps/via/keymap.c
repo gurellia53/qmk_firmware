91fe388f24082788f4dd4aa3bebd27d32e03a466
Andrew Gurik <agurik@agleader.com>
2022-03-21 10:22:58 -0500

    fix copyright

--- keyboards/navi10/keymaps/via/keymap.c
+++ keyboards/navi10/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 KnoblesseOblige
+/* Copyright 2022 gurellia53
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
